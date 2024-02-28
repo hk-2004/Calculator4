37a98bbf6d1065bf4ade76e43b87f0b917dfc46b
Harshi Kavalakuntla <harshik2000@gmail.com>
2024-02-28 18:23:53 -0500

    xcodecalculator

--- Calculator4/ViewController.swift
+++ Calculator4/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNum: UITextField!
+    @IBOutlet weak var secondNum: UITextField!
+    @IBOutlet weak var answer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Add(_ sender: UIButton) {
+        let Ans = Double(firstNum.text!)! + Double(secondNum.text!)!
+        answer.text = "Answer: \(Ans)"
+    }
+    @IBAction func Sub(_ sender: UIButton) {
+        let Ans = Double(firstNum.text!)! - Double(secondNum.text!)!
+        answer.text = "Answer: \(Ans)"
+    }
+    @IBAction func Mul(_ sender: UIButton) {
+        let Ans = Double(firstNum.text!)! * Double(secondNum.text!)!
+        answer.text = "Answer: \(Ans)"
+    }
+    
+    @IBAction func Div(_ sender: UIButton) {
+        let Ans = Double(firstNum.text!)! / Double(secondNum.text!)!
+        answer.text = "Answer: \(Ans)"
+    }
 }
 
